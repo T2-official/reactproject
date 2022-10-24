d1e4ceb85650c112d783d9386476c9808fcd343d
T2 <2686757907@qq.com>
2022-10-24 15:02:49 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// 导入页面组件
+import Login from './pages/Login'
+import Layout from './pages/Layout'
 
 function App() {
   return (
     <div className="App">
-
+      App
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// 引入scss
+import './index.scss'
+
+import 'antd/dist/antd.min.css'
+// 再导入全局样式文件，防止样式覆盖！
+import './index.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
