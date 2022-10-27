5ef93bcb89d49b61b532deafc4c26a678a3f3f7b
T2 <2686757907@qq.com>
2022-10-27 16:55:35 +0800

    结果

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,6 +1,11 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Table, Tag, Space, Card, Breadcrumb, Form, Button, Radio, DatePicker, Select } from 'antd'
+import { Table, Tag, Space, Card, Breadcrumb, Form, Button, Radio, DatePicker, Select, Popconfirm } from 'antd'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+
+import { http } from '@/utils'
+import { history } from '@/utils/history'
+
 import img404 from '@/assets/error.png'
 import 'moment/locale/zh-cn'
 import locale from 'antd/es/date-picker/locale/zh_CN'
@@ -10,6 +15,80 @@ const { Option } = Select
 const { RangePicker } = DatePicker
 
 const Article = () => {
+  // 获取频道列表
+  const [channels, setChannels] = useState([])
+  useEffect(() => {
+    async function fetchChannels() {
+      const res = await http.get('/channels')
+      setChannels(res.data.channels)
+    }
+    fetchChannels()
+  }, [])
+
+  // 文章列表数据管理
+  const [article, setArticleList] = useState({
+    list: [],
+    count: 0
+  })
+
+  // 参数管理
+  const [params, setParams] = useState({
+    page: 1,
+    per_page: 10
+  })
+
+  // 发送接口请求
+  useEffect(() => {
+    async function fetchArticleList() {
+      const res = await http.get('/mp/articles', { params })
+      const { results, total_count } = res.data
+      setArticleList({
+        list: results,
+        count: total_count
+      })
+    }
+    fetchArticleList()
+  }, [params])    // 这个依赖项十分巧妙的保证了分页反复请求
+
+  // 筛选功能
+  const onSearch = values => {
+    const { status, channel_id, date } = values
+    // 格式化表单数据
+    const _params = {}
+    // 格式化status
+    _params.status = status
+    if (channel_id) {
+      _params.channel_id = channel_id
+    }
+    if (date) {
+      _params.begin_pubdate = date[0].format('YYYY-MM-DD')
+      _params.end_pubdate = date[1].format('YYYY-MM-DD')
+    }
+    // 修改params参数 触发接口再次发起
+    setParams({
+      ...params,
+      ..._params
+    })  // 这样就能实现保留原params的基础上覆盖新值
+  }
+
+  // 删除回调
+  const delArticle = async (data) => {
+    await http.delete(`/mp/articles/${data.id}`)
+    // 更新列表
+    setParams({
+      page: 1,
+      per_page: 10
+    })
+  }
+
+  // 分页
+  const pageChange = async (page, pageSize) => {
+    setParams({
+      page,
+      per_page: pageSize
+    })
+  }
+
   const columns = [
     {
       title: '封面',
@@ -50,13 +129,21 @@ const Article = () => {
       render: data => {
         return (
           <Space size="middle">
-            <Button type="primary" shape="circle" icon={<EditOutlined />} />
-            <Button
-              type="primary"
-              danger
-              shape="circle"
-              icon={<DeleteOutlined />}
-            />
+            <Button type="primary" shape="circle" icon={<EditOutlined />}
+              onClick={() => history.push(`/home/publish?id=${data.id}`)} />
+            <Popconfirm
+              title="确认删除该条文章吗?"
+              onConfirm={() => delArticle(data)}
+              okText="确认"
+              cancelText="取消"
+            >
+              <Button
+                type="primary"
+                danger
+                shape="circle"
+                icon={<DeleteOutlined />}
+              />
+            </Popconfirm>
           </Space>
         )
       }
@@ -91,7 +178,7 @@ const Article = () => {
         }
         style={{ marginBottom: 20 }}
       >
-        <Form initialValues={{ status: null }}>
+        <Form initialValues={{ status: -1 }} onFinish={onSearch} >
           <Form.Item label="状态" name="status">
             <Radio.Group>
               <Radio value={null}>全部</Radio>
@@ -108,8 +195,11 @@ const Article = () => {
               defaultValue="选择频道"
               style={{ width: 120 }}
             >
-              <Option value="jack">Jack</Option>
-              <Option value="lucy">Lucy</Option>
+              {channels.map(item => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))}
             </Select>
           </Form.Item>
 
@@ -127,8 +217,19 @@ const Article = () => {
       </Card>
       <Card>
         {/* 列表区 */}
-        <Card title={`根据筛选条件共查询到 count 条结果：`}>
-          <Table rowKey="id" columns={columns} dataSource={data} />
+        <Card title={`根据筛选条件共查询到 ${article.count} 条结果：`}>
+          <Table
+            dataSource={article.list}
+            columns={columns}
+            rowKey={record => record.id}
+            pagination={{
+              position: ['bottomCenter'],
+              current: params.page,
+              pageSize: params.per_page,
+              onChange: pageChange,
+              total: article.count
+            }}
+          />
         </Card>
       </Card>
     </div>
--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -1,4 +1,143 @@
+import {
+  Card, Breadcrumb, Form, Button, Radio, Input, Upload, Space, Select
+} from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import { useEffect, useState, useRef } from "react";
+import { Link, useSearchParams } from 'react-router-dom'
+import { http } from "@/utils/http";
+import './index.scss'
+
+// 富文本编辑器
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+
+const { Option } = Select
+
 const Publish = () => {
-  return <div>Publish</div>
+
+  const form = useRef(null);
+  // 修改文章
+  const [params] = useSearchParams()
+  const articleId = params.get('id')
+
+  useEffect(() => {
+    async function getArticle() {
+      const res = await http.get(`/mp/articles/${articleId}`)
+      const { cover, ...formValue } = res.data
+      // 动态设置表单数据
+      form.setFieldsValue({ ...formValue, type: cover.type })
+      // 格式化封面图片数据
+      //const imageList = cover.images.map(url => ({ url }))
+      //setFileList(imageList)
+      //setMaxCount(cover.type)
+      //fileListRef.current = imageList
+    }
+    if (articleId) {
+      // 拉取数据回显
+      getArticle()
+    }
+  }, [articleId])
+
+  const onFinish = async (values) => {
+    // 数据的二次处理 重点是处理cover字段
+    console.log(values);
+  }
+
+  const [channels, setChannels] = useState([])
+  useEffect(() => {
+    async function fetchChannels() {
+      const res = await http.get('/channels')
+      setChannels(res.data.channels)
+    }
+    fetchChannels()
+  }, [])
+  return (
+    <div className="publish">
+      <Card
+        title={
+          <Breadcrumb separator=">">
+            <Breadcrumb.Item>
+              <Link to="/home">首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              {articleId ? '修改文章' : '发布文章'}
+            </Breadcrumb.Item>
+          </Breadcrumb>
+        }
+      >
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ content: '' }}
+          // 注意：此处需要为富文本编辑表示的 content 文章内容设置默认值
+          onFinish={onFinish}
+          ref={form}
+        >
+          <Form.Item
+            label="标题"
+            name="title"
+            rules={[{ required: true, message: '请输入文章标题' }]}
+          >
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+          </Form.Item>
+          <Form.Item
+            label="频道"
+            name="channel_id"
+            rules={[{ required: true, message: '请选择文章频道' }]}
+          >
+            <Select placeholder="请选择文章频道" style={{ width: 200 }}>
+              {channels.map(item => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="封面">
+            <Form.Item name="type">
+              <Radio.Group>
+                <Radio value={1}>单图</Radio>
+                <Radio value={3}>三图</Radio>
+                <Radio value={0}>无图</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Upload
+              name="image"
+              listType="picture-card"
+              className="avatar-uploader"
+              showUploadList
+            >
+              <div style={{ marginTop: 8 }}>
+                <PlusOutlined />
+              </div>
+            </Upload>
+          </Form.Item>
+          <Form.Item
+            label="内容"
+            name="content"
+            rules={[{ required: true, message: '请输入文章内容' }]}
+          >
+            <ReactQuill
+              className="publish-quill"
+              theme="snow"
+              placeholder="请输入文章内容"
+
+            />
+          </Form.Item>
+
+
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button size="large" type="primary" htmlType="submit">
+                {articleId ? '修改文章' : '发布文章'}
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
+
 export default Publish
